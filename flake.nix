24b67df538b489a2faaf0ecea16218c9ba1f2143
hugosenari <hugosenari@gmail.com>
2022-05-12 11:50:40 -0300

    chore: re index confutils

--- flake.nix
+++ flake.nix
@@ -7,6 +7,14 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."confutils-master".dir   = "master";
+  inputs."confutils-master".owner = "nim-nix-pkgs";
+  inputs."confutils-master".ref   = "master";
+  inputs."confutils-master".repo  = "confutils";
+  inputs."confutils-master".type  = "github";
+  inputs."confutils-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."confutils-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
